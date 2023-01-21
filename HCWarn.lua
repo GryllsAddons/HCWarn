9545afbdefafdfb43c8831625759d14352ba83bd
GryllsAddons <107083057+GryllsAddons@users.noreply.github.com>
2023-01-21 20:55:24 +0000

    Add files via upload

---
+++ HCWarn.lua
@@ -0,0 +1,48 @@
+local HCWarn = CreateFrame("Frame")
+HCWarn:SetPoint("CENTER", UIParent, "CENTER", 0, 0)
+HCWarn:SetHeight(10)
+HCWarn:SetWidth(300)
+
+HCWarn.player = HCWarn:CreateFontString("Status", "LOW", "GameFontNormal")
+HCWarn.player:SetFont(STANDARD_TEXT_FONT, 12, "OUTLINE")
+HCWarn.player:SetTextColor(255/255, 0/255, 0/255)
+HCWarn.player:SetAllPoints(HCWarn)
+
+HCWarn.target = HCWarn:CreateFontString("Status", "LOW", "GameFontNormal")
+HCWarn.target:SetFont(STANDARD_TEXT_FONT, 12, "OUTLINE")
+HCWarn.target:SetTextColor(255/255, 124/255, 10/255)
+HCWarn.target:SetPoint("TOP", HCWarn.player, "BOTTOM", 0, -5)
+
+local function pvp(unit)
+    local ispvp = UnitIsPVP(unit)
+    if unit == "player" then
+        if ispvp then
+            PlaySound("ReadyCheck","SFX")
+            HCWarn.player:SetText("YOU ARE PVP FLAGGED")
+        else
+            HCWarn.player:SetText("")
+        end
+    elseif unit == "target" then
+        if ispvp and UnitReaction("target", "player") <= 4 then -- hated to neutral
+            HCWarn.target:SetText("TARGET IS PVP FLAGGED")
+        else
+            HCWarn.target:SetText("")
+        end
+    end
+end
+
+local events = CreateFrame("Frame")
+events:RegisterEvent("PLAYER_TARGET_CHANGED")
+events:RegisterEvent("UNIT_FACTION", "player")
+events:RegisterEvent("UNIT_FACTION", "target")
+events:RegisterEvent("PLAYER_ENTERING_WORLD")
+
+events:SetScript("OnEvent", function()
+    if event == "PLAYER_TARGET_CHANGED" then
+        pvp("target")
+    elseif event == "PLAYER_ENTERING_WORLD" then
+        pvp("player")
+    else
+        pvp(arg1)
+    end
+end)
