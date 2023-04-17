9776a4c41d48b57a6255ac59996b2e0e2dcfbd3b
GryllsAddons <107083057+GryllsAddons@users.noreply.github.com>
2023-04-17 21:32:46 +0100

    Add files via upload

--- HCWarn.lua
+++ HCWarn.lua
@@ -1,101 +1,37 @@
-HCWarn_Settings = {
-    nointeract = false,
-}
-
 local HCWarn = CreateFrame("Frame")
-HCWarn:SetPoint("CENTER", UIParent, "CENTER", 0, 0)
-HCWarn:SetHeight(10)
-HCWarn:SetWidth(300)
+HCWarn:Hide()
+HCWarn:SetAllPoints(UIParent)
+HCWarn:SetBackdrop({edgeFile = "Interface\\AddOns\\HCWarn\\media\\border", edgeSize = 16})
+HCWarn:SetBackdropBorderColor(1, 0.25, 0)
 
 HCWarn.player = HCWarn:CreateFontString("Status", "LOW", "GameFontNormal")
 HCWarn.player:SetFont(STANDARD_TEXT_FONT, 12, "OUTLINE")
-HCWarn.player:SetTextColor(255/255, 0/255, 0/255)
-HCWarn.player:SetAllPoints(HCWarn)
+HCWarn.player:SetPoint("CENTER", HCWarn, "CENTER", 0, 50)
+HCWarn.player:SetText("YOU ARE PVP FLAGGED")
+HCWarn.player:SetTextColor(1, 0.25, 0)
 
-HCWarn.target = HCWarn:CreateFontString("Status", "LOW", "GameFontNormal")
-HCWarn.target:SetFont(STANDARD_TEXT_FONT, 12, "OUTLINE")
-HCWarn.target:SetTextColor(255/255, 124/255, 10/255)
-HCWarn.target:SetPoint("TOP", HCWarn.player, "BOTTOM", 0, -5)
-
-local function pvp(unit)    
-    if unit == "player" then
-        if UnitIsPVP("player") then
-            PlaySound("ReadyCheck","SFX")
-            HCWarn.player:SetText("YOU ARE PVP FLAGGED")
-        else
-            HCWarn.player:SetText("")
-        end
-    elseif unit == "target" then
-        if UnitIsPVP("target") then
-            if HCWarn_Settings.nointeract then                
-                ClearTarget()
-                UIErrorsFrame:AddMessage("Target is PVP flagged",1,0,0)
-            else
-                HCWarn.target:SetText("TARGET IS PVP FLAGGED")                
-            end
-        else
-            HCWarn.target:SetText("")
-        end
+function HCWarn:PvP()
+    if UnitIsPVP("player") then
+        SetCVar('MasterSoundEffects', 0)
+        SetCVar('MasterSoundEffects', 1)
+        HCWarn:Alert()
+        HCWarn:Show()
+    else
+        HCWarn:Hide()
     end
 end
 
-local function announceSetting()
-    if HCWarn_Settings.nointeract then
-        DEFAULT_CHAT_FRAME:AddMessage("HCWarn: You cannot interact with pvp flagged targets", 255/255, 0/255, 0/255)
-        UIErrorsFrame:AddMessage("You cannot interact with pvp flagged targets", 255/255, 0/255, 0/255)
-    else
-        DEFAULT_CHAT_FRAME:AddMessage("HCWarn: You can interact with pvp flagged targets", 255/255, 124/255, 10/255)
-        UIErrorsFrame:AddMessage("You can interact with pvp flagged targets", 255/255, 124/255, 10/255)
-    end
+function HCWarn:Alert()
+    PlaySoundFile("Interface\\AddOns\\HCWarn\\media\\"..HCWarn.faction..".ogg")
 end
 
-local function interactSetting()
-    -- set global interact variable for /stcast support
-    if HCWarn_Settings.nointeract then        
-        HCWarn_nointeract = true        
-    else
-        HCWarn_nointeract = nil        
-    end    
-end
-
-local function HCWarn_commands(msg, editbox)
-    if msg == "interact" then
-        if HCWarn_Settings.nointeract then
-            HCWarn_Settings.nointeract = false
-        else
-            HCWarn_Settings.nointeract = true
-        end
-        interactSetting()
-        announceSetting()
-    else
-        DEFAULT_CHAT_FRAME:AddMessage("HCWarn usage:")
-        DEFAULT_CHAT_FRAME:AddMessage("/hcwarn interact - toggle allowing interacting with targets that are pvp flagged")
-    end
-end
-
-local events = CreateFrame("Frame")
-events:RegisterEvent("ADDON_LOADED")
-events:RegisterEvent("PLAYER_TARGET_CHANGED")
-events:RegisterEvent("UNIT_FACTION", "player")
-events:RegisterEvent("UNIT_FACTION", "target")
-events:RegisterEvent("PLAYER_ENTERING_WORLD")
-
-events:SetScript("OnEvent", function()
-    if event == "ADDON_LOADED" then
-		if not HCWarn.loaded then
-            SLASH_HCWARN1 = "/hcwarn"
-            SLASH_HCWARN2 = "/hcw"
-            SlashCmdList["HCWARN"] = HCWarn_commands
-			DEFAULT_CHAT_FRAME:AddMessage("HCWarn loaded! /hcwarn")
-            interactSetting()
-            HCWarn.loaded = true            
-		end
-    elseif event == "PLAYER_TARGET_CHANGED" then
-        pvp("target")
-    elseif event == "PLAYER_ENTERING_WORLD" then
-        pvp("player")
-        announceSetting()
-    else
-        pvp(arg1)
+HCWarn:RegisterEvent("PLAYER_ENTERING_WORLD")
+HCWarn:RegisterEvent("UNIT_FACTION", "player")
+HCWarn:SetScript("OnEvent", function()
+    if event == "PLAYER_ENTERING_WORLD" then
+        HCWarn.faction = UnitFactionGroup("player")
+        HCWarn:PvP()
+    else        
+        HCWarn:PvP()
     end
 end)
